516329d6ea6ad524f8c5dedde0a7a8a58286af2d
stephanie <stephanie.menu.cda@gmail.com>
2022-07-29 15:35:39 +0200

    ajout navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+
+
+export default function navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Post</a>
+            </li>
+            <li class="nav-item ">
+            <a class="nav-link" href="#">inscription</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Connexion</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
